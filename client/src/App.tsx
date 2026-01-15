24dbec924ebaac4f2b5b4197e44142733f4939a9
initiology <36554174-initiology@users.noreply.replit.com>
2026-01-15 10:56:21 +0000

    Restored to 'd4bb41800461f8fb36651207d48db660208632d8'
    Replit-Restored-To: d4bb41800461f8fb36651207d48db660208632d8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Router, Switch, Route } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,7 +8,7 @@ import Home from "@/pages/home";
 import Department from "@/pages/department";
 import NotFound from "@/pages/not-found";
 
-function AppRouter() {
+function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -32,11 +31,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router hook={useHashLocation}>
-          <Toaster />
-          <Sidebar />
-          <AppRouter />
-        </Router>
+        <Toaster />
+        <Sidebar />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
